85ad73307450ba7df01c6f0e55fc776717f9dd44
borehelu <borehelu@gmail.com>
2024-04-14 18:23:53 +0300

    regiser models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Departments, Employees, EmergencyContact, Skills, EmployeeSkills, LeaveRequests, LeaveTypes, LeaveBalances
 
 # Register your models here.
+admin.site.register(Employees)
+admin.site.register(Departments)
+admin.site.register(EmergencyContact)
+admin.site.register(Skills)
+admin.site.register(EmployeeSkills)
+admin.site.register(LeaveTypes)
+admin.site.register(LeaveRequests)
+admin.site.register(LeaveBalances)
